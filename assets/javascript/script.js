1fc14ca6bc5711cbc22af3dd8fc6f1f429cfe58f
chasem <cpmcquown@gmail.com>
2021-06-02 20:00:26 -0700

    styling mostly complete, possibly finished. Intial logic was added as well

---
+++ assets/javascript/script.js
@@ -0,0 +1,58 @@
+// identify start button
+var startBtn = document.querySelector("#start");
+
+// time variable 
+var timer = 75; 
+
+//player score variable; player starts with 10 points
+var playerScore = 10;
+
+
+
+
+
+
+
+
+
+function startQuiz() {
+    
+    alert('Once you click "ok" the timer will begin. Each missed answer results in 10 seconds being subtracted from the timer... good luck!')
+
+    // code for timer should go below
+
+    // question 1 should be appended to h2 element
+
+    // buttons with text content of answers will be appeneded
+
+    // add conditional to check for right or wrong answer below
+
+    // add if statement to display either right or wring after player submits answer
+
+    // add to player score variable if answer is correct
+
+    // subtract from time count if player's answer is wrong 
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// add event listener to start button 
+startBtn.addEventListener("click", startQuiz)
+
+
